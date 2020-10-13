994d325f4d7f0db5e45e0ee0160610b73bbe3f01
Mark <marknoonmn@gmail.com>
2020-10-14 02:05:36 +0300

    update

---
+++ js/default.js
@@ -0,0 +1,114 @@
+
+var run = 1;
+const canvas = document.getElementById("bg");
+const context=canvas.getContext("2d");
+canvas.width=1960;
+canvas.height=1080;
+document.getElementById('body').onload=BGLOAD();
+async function LoadGallery()
+{
+        var i=1,j=1;
+
+        var div = document.createElement('div');
+        div.className="imgrid";
+        div.id="imgrid";
+    
+        document.getElementById("body").appendChild(div);
+        
+
+            var path = "./prij/";
+            while(run !=0)
+            { 
+                j=1;
+                    var dir = path+String(i);
+                    while(j<12 && run!=0)
+                    {
+                        
+                        
+                            var file = dir+"/"+String(j)+".png";
+                            
+
+                            //img.addEventListener('load', Place(d,file));
+                            
+                            //img.addEventListener('error', Error(file));
+                            var result="";
+                            await Check(file).then(res => oof(res));
+                            j++;                                
+                    }    
+                i++;
+                
+            }
+            $("#imgrid img").each(function () {
+               // this.style.maxHeight=(Math.floor(Math.random() * 100) + 50)+'%';
+               // this.style.maxWidth=(Math.floor(Math.random() * 100) + 50)+'%';
+            });
+        }
+function oof(res)
+{
+    if(res != false)
+    {
+        var img= document.createElement("img");
+        img.src=res;
+
+        div = document.getElementById("imgrid");
+        div.appendChild(img);
+    }
+    else
+    {
+        run = 0;
+    }
+}
+function Check(file)
+{
+    var oReq = new XMLHttpRequest();
+    return new Promise((resolve, reject) =>
+    {
+        oReq.onload = (e) => {
+            if(oReq.status==404)
+            {
+                resolve(false);
+            }
+            else
+            {
+                resolve(file);
+            }};
+        oReq.open("GET", file);
+        oReq.send();
+    });
+}
+function reqListener() 
+{
+    //if(this.status != 404)
+    //{
+        
+    //}
+}
+function Place(d , img)
+{
+    div = document.getElementById("imgrid");
+    div.appendChild(img);         
+}
+function Error(file)
+{
+   //alert(file);
+   v=0;
+}
+
+function BGLOAD()
+{
+    const image = new Image();
+    image.src=currentFrame(0);
+    image.onload=Render(image);
+}
+function currentFrame(i)
+{
+    var str = "" + i
+    var pad = "0000000"
+    var ans = pad.substring(0, pad.length - str.length) + str
+    return "res/bg/BG."+ans+".png";
+    //return `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${i.toString().padStart(4, '0')}.jpg`;
+}
+function Render(image)
+{
+    context.drawImage(image,0,0);
+}

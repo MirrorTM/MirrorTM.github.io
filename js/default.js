4c325f27bebf50775d64b9ddfb5f91b0ff725eba
Mark <marknoonmn@gmail.com>
2020-10-14 03:22:03 +0300

    Update default.js

--- js/default.js
+++ js/default.js
@@ -115,11 +115,11 @@ function Error(file)
 function currentFrame(i)
 {
     
-    //var str = "" + i
-    //var pad = "0000000"
-    //var ans = pad.substring(0, pad.length - str.length) + str
-    //return "res/bg/BG."+ans+".png";
-    return `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${i.toString().padStart(4, '0')}.jpg`;
+    var str = "" + i
+    var pad = "0000000"
+    var ans = pad.substring(0, pad.length - str.length) + str
+    return "res/bg/BG."+ans+".png";
+    //return `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${i.toString().padStart(4, '0')}.jpg`;
 }
 function Render()
 {
@@ -134,7 +134,7 @@ function Render()
         
         idex++;
         
-        if(idex==148)
+        if(idex==180)
         {
             idex=0;
         }
